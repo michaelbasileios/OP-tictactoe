40c39048ae7d5c39d3338ead38906f0cd94df794
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2024-12-26 11:58:13 -0600

    Complete gameboard function iteration

--- script.js
+++ script.js
@@ -6,7 +6,27 @@ function Gameboard() {
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
-            board[i].push(Cell());
+            board[i].push(Square());
         }
     }
+
+    const getBoard = () => board;
+
+    const markSquare = (column, player) => {
+        const availableSquares = board.filter((row) => 
+        row[column].getValue() === 0).map(row => row[column]);
+
+        if(!availableSquares.length) return;
+
+        const lowestRow = availableSquares.length - 1;
+        board[lowestRow][column].addMark(player);
+    };
+
+    const renderBoard = () => {
+        const boardWithMarks = board.map((row) =>
+        row.map((square) = square.getValue()))
+        console.log(boardWithMarks);
+    };
+
+    return {getBoard, markSquare, renderBoard};
 }
