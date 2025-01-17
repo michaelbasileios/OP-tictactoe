86e5c3a79d648859fa319287815cbe168a8533c2
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2025-01-17 10:44:30 -0600

    Add CSS reset

--- script.js
+++ script.js
@@ -1,92 +1,88 @@
 function Gameboard() {
-    const rows = 3;
-    const columns = 3;
+    
     const board = [];
-
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push(Square());
-        }
-    }
 
     const getBoard = () => board;
 
-    const markSquare = (column, player) => {
-        const availableSquares = board.filter((row) => 
-        row[column].getValue() === 0).map(row => row[column]);
+    // const markSquare = (column, player) => {
+    //     const availableSquares = board.filter((row) => 
+    //     row[column].getValue() === 0).map(row => row[column]);
 
-        if(!availableSquares.length) return;
+    //     if(!availableSquares.length) return;
 
-        const lowestRow = availableSquares.length - 1;
-        board[lowestRow][column].addMark(player);
-    };
+    //     const lowestRow = availableSquares.length - 1;
+    //     board[lowestRow][column].addMark(player);
+    // };
 
-    const renderBoard = () => {
-        const boardWithMarks = board.map((row) =>
-        row.map((square) = square.getValue()))
-        console.log(boardWithMarks);
-    };
+    // const renderBoard = () => {
+    //     const boardWithMarks = board.map((row) =>
+    //     row.map((square) = square.getValue()))
+    //     console.log(boardWithMarks);
+    // };
 
-    return {getBoard, markSquare, renderBoard};
+    // return {getBoard, markSquare, renderBoard};
 }
 
-function Square() {
-    let value = 0;
-
-    const addMark = (player) => {
-        value = player;
-    };
-
-    const getValue = () => value;
-
-    return {
-        addMark,
-        getValue
-    };
+function createPlayer (name, mark) {
+    return {name, mark};
 }
 
-function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
-    const board = Gameboard();
+// function Square() {
+//     let value = 0;
 
-    const players = [
-        {
-            name: playerOneName,
-            mark: 1
-        },
-        {
-            name: playerTwoName,
-            mark: 2
-        }
-    ];
+//     const addMark = (player) => {
+//         value = player;
+//     };
 
-    let activePlayer = players[0];
+//     const getValue = () => value;
 
-    const switchPlayerTurn = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : player[0];
-    };
-    const getActivePlayer = () => activePlayer;
+//     return {
+//         addMark,
+//         getValue
+//     };
+// }
 
-    const renderNewRound = () => {
-        board.renderBoard();
-        console.log(`${getActivePlayer().name}'s turn.`);
-    };
+// function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
+//     const board = Gameboard();
 
-    const playRound = (row, column) => {
-        console.log(
-            `Adding ${getActivePlayer().name}'s mark to row ${row} column ${column}`
-        );
-        board.markSquare(column, getActivePlayer().mark);
+//     const players = [
+//         {
+//             name: playerOneName,
+//             mark: 1
+//         },
+//         {
+//             name: playerTwoName,
+//             mark: 2
+//         }
+//     ];
 
-        // ADD WIN LOGIC HERE
+//     let activePlayer = players[0];
 
-        switchPlayerTurn();
-        renderNewRound();
-    };
+//     const switchPlayerTurn = () => {
+//         activePlayer = activePlayer === players[0] ? players[1] : player[0];
+//     };
+//     const getActivePlayer = () => activePlayer;
 
-    renderNewRound();
+//     const renderNewRound = () => {
+//         board.renderBoard();
+//         console.log(`${getActivePlayer().name}'s turn.`);
+//     };
 
-    return {
-        playRound, getActivePlayer, getBoard: board.getBoard
-    };
-}
+//     const playRound = (row, column) => {
+//         console.log(
+//             `Adding ${getActivePlayer().name}'s mark to row ${row} column ${column}`
+//         );
+//         board.markSquare(column, getActivePlayer().mark);
+
+//         // ADD WIN LOGIC HERE
+
+//         switchPlayerTurn();
+//         renderNewRound();
+//     };
+
+//     renderNewRound();
+
+//     return {
+//         playRound, getActivePlayer, getBoard: board.getBoard
+//     };
+// }
